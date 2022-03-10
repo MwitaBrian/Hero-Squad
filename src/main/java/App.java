eb1ea1039aa62a5c4aa699ca88051e772215ece3
MwitaBrian <brianmwita13@gmail.com>
2022-03-10 12:10:38 +0300

    Create Template of the App.java file

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,2 +1,16 @@
+import static spark.Spark.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import java.util.HashMap;
+
+import java.util.Map;
+
+import spark.ModelAndView;
+import spark.TemplateEngine;
+
 public class App {
+    public static void main(String[] args) {
+
+    }
 }
