b4a0818623197ade60c98e3369efa828cad85013
MwitaBrian <brianmwita13@gmail.com>
2022-03-10 21:07:34 +0300

    Add images

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,10 +7,15 @@ import java.util.HashMap;
 import java.util.Map;
 
 import spark.ModelAndView;
-import spark.TemplateEngine;
+import spark.template.handlebars.HandlebarsTemplateEngine;
 
 public class App {
     public static void main(String[] args) {
+        staticFileLocation("/public");
 
+        get("/", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            return new ModelAndView(model, "index.hbs");
+        }, new HandlebarsTemplateEngine());
     }
 }
